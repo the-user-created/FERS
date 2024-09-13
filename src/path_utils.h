643ede3f7e122e783aa8a30b0d8eb64e09e65828
David Young <davidsamuelyoung@protonmail.com>
2024-09-13 13:01:16 +0200

    refactor(path_utils): simplify template functions

--- src/path_utils.h
+++ src/path_utils.h
@@ -21,9 +21,12 @@ namespace path
 	};
 }
 
-//The interpolation functions are implemented as template functions to ease adding more functions to both rotation and motion classes
+// TODO: Can use concepts with these template functions
 
-//Static "interpolation" function - the path is at the same point all the time
+// The interpolation functions are implemented as template functions
+// to ease adding more functions to both rotation and motion classes
+
+// Static "interpolation" function - the path is at the same point all the time
 template <typename T>
 void getPositionStatic(T& coord, const std::vector<T>& coords)
 {
@@ -31,78 +34,58 @@ void getPositionStatic(T& coord, const std::vector<T>& coords)
 	coord = coords[0];
 }
 
-//Linear interpolation function
+// Linear interpolation function
 template <typename T>
 void getPositionLinear(RS_FLOAT t, T& coord, const std::vector<T>& coords)
 {
-	T s_key;
-	s_key = 0;
+	T s_key = {};
 	s_key.t = t;
-	typename std::vector<T>::const_iterator xrp;
-	xrp = upper_bound(coords.begin(), coords.end(), s_key);
-	//Check if we are over one of the end points
-	if (xrp == coords.begin())
-	{
-		coord = *xrp; //We are at the left endpoint
-	}
-	else if (xrp == coords.end())
-	{
-		coord = *(xrp - 1); //We are at the right endpoint
-	}
+	auto xrp = std::upper_bound(coords.begin(), coords.end(), s_key);
+
+	if (xrp == coords.begin()) { coord = *xrp; }
+	else if (xrp == coords.end()) { coord = *(xrp - 1); }
 	else
 	{
-		//We are at neither endpoint - perform linear interpolation
 		int xri = xrp - coords.begin();
 		int xli = xri - 1;
-
 		RS_FLOAT iw = coords[xri].t - coords[xli].t;
 		RS_FLOAT rw = (coords[xri].t - t) / iw;
 		RS_FLOAT lw = 1 - rw;
-		//Insert the interpolated values in coord
 		coord = coords[xri] * lw + coords[xli] * rw;
 	}
-	//Set the time part of the coordinate
 	coord.t = t;
 }
 
-//Cubic spline interpolation function
-//The method used (but not the code) is from
-//Numerical Recipes in C, Second Edition by Press, et al. pages 114-116
+// Cubic spline interpolation function
+// The method used (but not the code) is from
+// Numerical Recipes in C, Second Edition by Press, et al. pages 114-116
 template <typename T>
 void getPositionCubic(RS_FLOAT t, T& coord, const std::vector<T>& coords, const std::vector<T>& dd)
 {
-	T s_key;
-	s_key = 0;
+	T s_key = {};
 	s_key.t = t;
-	typename std::vector<T>::const_iterator xrp;
-	//Check that we are finalized, if not, complain
-	xrp = upper_bound(coords.begin(), coords.end(), s_key);
-	//Check if we are over one of the end points
-	if (xrp == coords.begin())
-	{
-		coord = *xrp; //We are at the left endpoint
-	}
-	else if (xrp == coords.end())
-	{
-		coord = *(xrp - 1); //We are at the right endpoint
-	}
+	auto xrp = std::upper_bound(coords.begin(), coords.end(), s_key);
+
+	if (xrp == coords.begin()) { coord = *xrp; }
+	else if (xrp == coords.end()) { coord = *(xrp - 1); }
 	else
 	{
-		//We are at neither endpoint - perform cubic spline interpolation
 		int xri = xrp - coords.begin();
 		int xli = xri - 1;
-		const RS_FLOAT xrd = coords[xri].t - t, xld = t - coords[xli].t, iw = coords[xri].t - coords[xli].t, iws = iw *
-			               iw / 6.0;
-		RS_FLOAT a = xrd / iw, b = xld / iw, c = (a * a * a - a) * iws, d = (b * b * b - b) * iws;
+		const RS_FLOAT xrd = coords[xri].t - t, xld = t - coords[xli].t;
+		const RS_FLOAT iw = coords[xri].t - coords[xli].t;
+		const RS_FLOAT iws = iw * iw / 6.0;
+		RS_FLOAT a = xrd / iw, b = xld / iw;
+		RS_FLOAT c = (a * a * a - a) * iws;
+		RS_FLOAT d = (b * b * b - b) * iws;
 		coord = coords[xli] * a + coords[xri] * b + dd[xli] * c + dd[xri] * d;
 	}
-	//Set the time part of the coordinate
 	coord.t = t;
 }
 
-//Finalize function to calculate vector of second derivatives
-//The method used (but not the code) is from
-//Numerical Recipes in C, Second Edition by Press, et al. pages 114-116
+// Finalize function to calculate vector of second derivatives
+// The method used (but not the code) is from
+// Numerical Recipes in C, Second Edition by Press, et al. pages 114-116
 template <typename T>
 void finalizeCubic(std::vector<T>& coords, std::vector<T>& dd)
 {
@@ -110,21 +93,22 @@ void finalizeCubic(std::vector<T>& coords, std::vector<T>& dd)
 	std::vector<T> tmp(size);
 	dd.resize(size);
 
-	//Set the second derivative at the end points to zero
 	dd[0] = 0;
 	dd[size - 1] = 0;
-	//Forward pass of calculating the second derivatives at each point
+
 	for (int i = 1; i < size - 1; i++)
 	{
-		T yrd = coords[i + 1] - coords[i], yld = coords[i] - coords[i - 1];
-		RS_FLOAT xrd = coords[i + 1].t - coords[i].t, xld = coords[i].t - coords[i - 1].t;
+		T yrd = coords[i + 1] - coords[i];
+		T yld = coords[i] - coords[i - 1];
+		RS_FLOAT xrd = coords[i + 1].t - coords[i].t;
+		RS_FLOAT xld = coords[i].t - coords[i - 1].t;
 		RS_FLOAT iw = coords[i + 1].t - coords[i - 1].t;
 		RS_FLOAT si = xld / iw;
 		T p = dd[i - 1] * si + 2.0;
 		dd[i] = (si - 1.0) / p;
 		tmp[i] = ((yrd / xrd - yld / xld) * 6.0 / iw - tmp[i - 1] * si) / p;
 	}
-	//Second (backward) pass of calculation
+
 	for (int i = size - 2; i >= 0; --i) { dd[i] = dd[i] * dd[i + 1] + tmp[i]; }
 }
 
