5c6850c2afecc4c0e0073eaf00972baed9985957
David Young <davidsamuelyoung@protonmail.com>
2025-09-16 20:17:05 +0200

    feat: add Prettier and ESLint configuration files with ignore patterns

---
+++ packages/fers-ui/prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+    semi: true,
+    trailingComma: 'es5',
+    singleQuote: true,
+    printWidth: 80,
+    tabWidth: 4,
+};
+
+export default config;
