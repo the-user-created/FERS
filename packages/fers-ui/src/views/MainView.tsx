00e9c20e79a6dbebaa803754dd6b51fe5b13702b
David Young <davidsamuelyoung@protonmail.com>
2025-09-16 16:12:40 +0200

    feat(ui): add initial tauri and react ui

---
+++ packages/fers-ui/src/views/MainView.tsx
@@ -0,0 +1,22 @@
+import {Box} from '@mui/material';
+import {Canvas} from '@react-three/fiber';
+import WorldView from '@/components/WorldView';
+
+/**
+ * MainView contains the 3D rendering canvas.
+ */
+export function MainView() {
+    return (
+        <Box
+            sx={{
+                flex: 1,
+                height: '100%',
+                backgroundColor: 'black',
+            }}
+        >
+            <Canvas camera={{position: [10, 10, 10], fov: 25}}>
+                <WorldView/>
+            </Canvas>
+        </Box>
+    );
+}
